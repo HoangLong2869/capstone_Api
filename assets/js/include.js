1434071e99023bcf2fcd39cf57ad9dde72df53c5
Nguyen Anh <nguyenanh1904.vn@gmail.com>
2023-07-06 22:23:48 +0700

    10:23

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,24 +1,28 @@
 function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    z = document.getElementsByTagName("*");
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        file = elmnt.getAttribute("w3-include-html");
-        if (file) {
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status == 200) { elmnt.innerHTML = this.responseText; }
-                    if (this.status == 404) { elmnt.innerHTML = "Page not found."; }
-                    elmnt.removeAttribute("w3-include-html");
-                    includeHTML();
-                }
-            }
-            xhttp.open("GET", file, true);
-            xhttp.send();
-            return;
+  let z, file;
+  const xhttp = new XMLHttpRequest();
+  z = document.getElementsByTagName("*");
+  for (let i = 0; i < z.length; i++) {
+    const elmnt = z[i];
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
         }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
     }
+  }
 }
 
-includeHTML()
+includeHTML();
